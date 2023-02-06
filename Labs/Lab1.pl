1b785726e80e6f19cbaefce4c6281b20298552ca
Quinton98 <123001866+Quinton98@users.noreply.github.com>
2023-02-06 18:24:36 +0000

    1hw

---
+++ Labs/Lab1.pl
@@ -0,0 +1,31 @@
+
+#Labs 1 2/6/2023
+
+print("What is your first name?\n");
+$input = <>;
+chomp($input);
+print("Your name is $input\n");
+
+
+print("What is your last name?\n");
+$input = <>;
+chomp($input);
+print("Your last name is $input\n");
+
+
+print("What is your age?\n");
+$input = <>;
+chomp($input);
+print("Your age is $input\n");
+
+
+print("What is your date of birth?\n");
+$input = <> ;
+chomp($input);
+print("Your date of birth is $input\n");
+
+
+print("What is your hair color?\n");
+$input = <>;
+chomp($input);
+print("Your hair color is $input\n");
