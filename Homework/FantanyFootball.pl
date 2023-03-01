b4fa42bc556b31209ce6d88d820708b7bde35d33
Quinton98 <123001866+Quinton98@users.noreply.github.com>
2023-03-01 17:11:13 +0000

    3/1/23

---
+++ Homework/FantanyFootball.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+
+%player = ( player1 => "5 wins", player2 => "2 wins", player3 => "0 wins", player4 => "1 wins", player5 => "8 wins");
+
+print(%player);
+
---
+++ Homework/Football.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+print("Do you want to add or delete a player? ");
+$answer = <>;
+chomp($answer);
+print("What player do you want to add? ");
+$player = <>;
+chomp($player);
+
+
+%players = ( Kyle => "5 wins", Matt => "3 wins", Josh => "9 wins", Cam => "4 wins", Brandon => "2 wins");
+for(sort keys %players){
+print("Key: $_, value: $players{$_}\n");
+}
+print("\n");
+
+$players{Quinton} = "3 wins";
+for (sort keys %players){
+print("Key: $_, value: $players{$_}\n");
+}
+print("\n");
+
+print("Do you want to add or delete a player?");
+$answer = <>;
+chomp($answer);
+print("What player do you want to delete?");
+$player = <>;
+chomp($player); 
+
+delete $players{Quinton};
+for(sort keys %players){
+print("Key: $_, value: $players{$_}\n");
+}
+print("\n");
